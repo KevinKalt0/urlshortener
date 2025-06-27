8a1da4386d36b21e07190c3f47c9fa5714122cfe
Ryskell <samhauho2001@gmail.com>
2025-06-27 12:00:59 +0200

    ajout de la commande 'create' pour raccourcir une URL depuis le terminal

--- cmd/cli/create.go
+++ cmd/cli/create.go
@@ -15,7 +15,7 @@ import (
 )
 
 // TODO : Faire une variable longURLFlag qui stockera la valeur du flag --url
-
+var longURLFlag string
 // CreateCmd représente la commande 'create'
 var CreateCmd = &cobra.Command{
 	Use:   "create",
@@ -26,26 +26,39 @@ Exemple:
   url-shortener create --url="https://www.google.com/search?q=go+lang"`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO 1: Valider que le flag --url a été fourni.
-
+		if _, err := url.ParseRequestURI(longURLFlag); err != nil {
+			fmt.Println("L'URL fournie n'est pas valide.")
+			os.Exit(1)
+		}
 		// TODO Validation basique du format de l'URL avec le package url et la fonction ParseRequestURI
 		// si erreur, os.Exit(1)
 
 		// TODO : Charger la configuration chargée globalement via cmd.cfg
-
+		
 		// TODO : Initialiser la connexion à la base de données SQLite.
+
+		db, err := gorm.Open(sqlite.Open("url_shortener.db"), &gorm.Config{})
+		if err != nil {
+			log.Fatalf("FATAL: Impossible de se connecter à la base : %v", err)
+		}
 
 		sqlDB, err := db.DB()
 		if err != nil {
-			log.Fatalf("FATAL: Échec de l'obtention de la base de données SQL sous-jacente: %v", err)
+			log.Fatalf("FATAL: Impossible d'obtenir l'instance SQL : %v", err)
 		}
+		defer sqlDB.Close()
 
 		// TODO S'assurer que la connexion est fermée à la fin de l'exécution de la commande
 		
 		// TODO : Initialiser les repositories et services nécessaires NewLinkRepository & NewLinkService
-
+		linkRepo := repository.NewLinkRepository(db)
+		linkService := services.NewLinkService(linkRepo)
 		// TODO : Appeler le LinkService et la fonction CreateLink pour créer le lien court.
 		// os.Exit(1) si erreur
-
+		link, err := linkService.CreateLink(longURLFlag)
+		if err != nil {
+			log.Fatalf("FATAL: Erreur lors de la création du lien court : %v", err)
+		}
 		fullShortURL := fmt.Sprintf("%s/%s", cfg.Server.BaseURL, link.ShortCode)
 		fmt.Printf("URL courte créée avec succès:\n")
 		fmt.Printf("Code: %s\n", link.ShortCode)
@@ -57,7 +70,9 @@ Exemple:
 // Il est utilisé pour définir les flags que cette commande accepte.
 func init() {
 	// TODO : Définir le flag --url pour la commande create.
-
+	CreateCmd.Flags().StringVar(&longURLFlag, "url", "", "URL longue à raccourcir")
+	CreateCmd.MarkFlagRequired("url")
+	cmd2.RootCmd.AddCommand(CreateCmd)
 	// TODO :  Marquer le flag comme requis
 
 	// TODO : Ajouter la commande à RootCmd
